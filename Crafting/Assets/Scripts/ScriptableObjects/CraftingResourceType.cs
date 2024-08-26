e6e8be1fdab17f6f3928a8f48c122c849ba33d23
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-08-26 23:44:49 +0200

    Add sprite to resource type

--- Crafting/Assets/Scripts/ScriptableObjects/CraftingResourceType.cs
+++ Crafting/Assets/Scripts/ScriptableObjects/CraftingResourceType.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName ="Crafting resource type",fileName ="New crafting resource type")]
+[CreateAssetMenu(menuName ="Crafting resource type",fileName ="New crafting resource ")]
 public class CraftingResourceType : ScriptableObject
 {
-
+    public Sprite ResourceSprite => _resourceSprite;
+    [SerializeField] Sprite _resourceSprite;
 }
