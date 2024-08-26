98d654c1f480976957b078030c2a4af027374855
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-08-26 16:35:57 +0200

    Create scriptable objects scripts for crafting recipe and resources.

---
+++ Crafting/Assets/Scripts/ScriptableObjects/CraftingRecipe.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class CraftingRecipe : ScriptableObject
+{
+    public struct CraftingRecipeShort
+    {
+        public CraftingResourceType[] resourceTypes;
+        public int[] resourcesNum;
+    }
+
+    public List<CraftingResourceType> CraftingResources=>_craftingResources;
+    [SerializeField] List<CraftingResourceType> _craftingResources;
+    public GameObject Result=>_result;
+    [SerializeField] GameObject _result;
+    public Sprite ResultSprite => _resultSprite;
+    [SerializeField] Sprite _resultSprite;
+}
---
+++ Crafting/Assets/Scripts/ScriptableObjects/CraftingResourceType.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Crafting resource type",fileName ="New crafting resource type")]
+public class CraftingResourceType : ScriptableObject
+{
+
+}
